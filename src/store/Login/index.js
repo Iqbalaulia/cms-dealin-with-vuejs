d71b218fe246ed7e9c17983ca539a60bba016bbd
iqbalaulia <iqbal.developer16@gmail.com>
2023-05-26 00:43:49 +0700

    user pass

--- src/store/Login/index.js
+++ src/store/Login/index.js
@@ -1,36 +1,38 @@
+/** @format */
+
 const state = {
-  data: {
-    form: {
-      username: "",
-      password: "",
-    },
-    isLoading: false,
-  },
+	data: {
+		form: {
+			username: "admin",
+			password: "admin123",
+		},
+		isLoading: false,
+	},
 };
 
 const mutations = {
-  changeLogin(state, payload) {
-    state.data = Object.assign({}, state.data, payload);
-  },
+	changeLogin(state, payload) {
+		state.data = Object.assign({}, state.data, payload);
+	},
 };
 
 const actions = {
-  // async handleSubmitLogin({ commit, state }) {
-  //   commit("changeLogin", {
-  //     isLoading: true,
-  //   });
-  //   const { data } = state;
-  //   let formData = {
-  //     username: data.form.username,
-  //     password: data.form.password,
-  //   };
-  //   const result = await
-  // },
+	// async handleSubmitLogin({ commit, state }) {
+	//   commit("changeLogin", {
+	//     isLoading: true,
+	//   });
+	//   const { data } = state;
+	//   let formData = {
+	//     username: data.form.username,
+	//     password: data.form.password,
+	//   };
+	//   const result = await
+	// },
 };
 
 export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions,
+	namespaced: true,
+	state,
+	mutations,
+	actions,
 };
